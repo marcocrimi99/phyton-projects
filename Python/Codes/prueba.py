aedb1b1797fc8ca83c35ebdf258f971583dd66f0
Marco Crimi <100549943+marcocrimi99@users.noreply.github.com>
2023-07-19 17:07:49 -0300

    Python notes

---
+++ Python/Codes/prueba.py
@@ -0,0 +1,32 @@
+from openpyxl import sheet as sh
+
+#h = 5
+#for h in range(10):
+#    print(h * 10)
+    
+#while h != 10:
+#    print (h * 11)
+#    h += 1
+#    if h * 11 == 77:
+#            print('setenta y siete')
+#            break
+#h = 8
+#while h != 10:
+#    print (h * 11)
+#    h += 1
+
+print('   ')
+
+a = [1, 2, 4]
+b = [2, 4, 6]
+print(sum(a))
+c = sum(a+b)
+print(c)
+print(range(5))
+print(sum(list(range(5)))
+
+print('   ')
+
+for ch in (map(sum, zip(a,b))):
+    print(ch)
+
